3fb5b5d808d9c39a7f18c2466fadb7358d449e9f
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-03-16 19:02:48 +0100

    Revert "fix: remove provider"
    This reverts commit def780f3398b2367473c1216ca61ef8e585247ea.

--- src/main/kotlin/parsotongue/providers/LexerProviders.kt
+++ src/main/kotlin/parsotongue/providers/LexerProviders.kt
@@ -2,9 +2,14 @@ package parsotongue.providers
 
 import parsotongue.lexer.LanguageLexer
 import parsotongue.lexer.Lexer
+import parsotongue.lexer.ParallelLexer
 
 interface LexerProvider : Provider<String, Lexer>
 
 object LanguageLexerProvider : LexerProvider {
     override fun get(parameter: String): Lexer = LanguageLexer(parameter)
+}
+
+object ParallelLexerProvider : LexerProvider {
+    override fun get(parameter: String): Lexer = ParallelLexer(parameter)
 }
