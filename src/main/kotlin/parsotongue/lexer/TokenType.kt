a3cd30b5808451284cf6b3cd470f40ec99d54200
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-03-16 13:38:39 +0100

    feat: add supported token types

--- src/main/kotlin/parsotongue/lexer/TokenType.kt
+++ src/main/kotlin/parsotongue/lexer/TokenType.kt
@@ -1,4 +1,22 @@
 package parsotongue.lexer
 
 enum class TokenType {
+    // keywords
+    VAR, FUNCTION, IF, ELSE, RETURN,
+
+    // operators
+    PLUS, MINUS, MULTIPLY, DIVIDE, MODULO,
+    LESS_THAN, GREATER_THAN, LESS_EQUAL, GREATER_EQUAL, EQUAL_EQUAL, NOT_EQUAL,
+
+    // symbols
+    ASSIGN, SEMICOLON, LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA,
+
+    // literals
+    INTEGER, STRING,
+
+    // identifiers
+    IDENTIFIER,
+
+    // other
+    EOF
 }
