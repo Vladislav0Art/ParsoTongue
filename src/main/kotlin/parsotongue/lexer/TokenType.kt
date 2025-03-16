4cc59ce19f2b15472bb0228d19660ff1bb506018
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2025-03-16 15:48:57 +0100

    refactor: add pretty printing of token type names

--- src/main/kotlin/parsotongue/lexer/TokenType.kt
+++ src/main/kotlin/parsotongue/lexer/TokenType.kt
@@ -1,55 +1,55 @@
 package parsotongue.lexer
 
 
+sealed class TokenType {
+    override fun toString(): String = this::class.simpleName.orEmpty()
 
-
-sealed interface TokenType {
     // keywords
-    sealed interface Keyword : TokenType {
-        object VAR : Keyword
-        object FUNCTION : Keyword
-        object IF : Keyword
-        object ELSE : Keyword
-        object RETURN : Keyword
+    sealed class Keyword : TokenType() {
+        object VAR : Keyword()
+        object FUNCTION : Keyword()
+        object IF : Keyword()
+        object ELSE : Keyword()
+        object RETURN : Keyword()
     }
 
-    sealed interface Operator : TokenType {
-        object PLUS : Operator
-        object MINUS : Operator
-        object MULTIPLY : Operator
-        object DIVIDE : Operator
-        object MODULO : Operator
-        object LESS_THAN : Operator
-        object GREATER_THAN : Operator
-        object LESS_EQUAL : Operator
-        object GREATER_EQUAL : Operator
-        object EQUAL_EQUAL : Operator
-        object NOT_EQUAL : Operator
+    sealed class Operator : TokenType() {
+        object PLUS : Operator()
+        object MINUS : Operator()
+        object MULTIPLY : Operator()
+        object DIVIDE : Operator()
+        object MODULO : Operator()
+        object LESS_THAN : Operator()
+        object GREATER_THAN : Operator()
+        object LESS_EQUAL : Operator()
+        object GREATER_EQUAL : Operator()
+        object EQUAL_EQUAL : Operator()
+        object NOT_EQUAL : Operator()
     }
 
     // symbols
-    sealed interface Symbol : TokenType {
-        object ASSIGN : Symbol
-        object SEMICOLON : Symbol
-        object LEFT_PAREN : Symbol
-        object RIGHT_PAREN : Symbol
-        object LEFT_BRACE : Symbol
-        object RIGHT_BRACE : Symbol
-        object COMMA : Symbol
+    sealed class Symbol : TokenType() {
+        object ASSIGN : Symbol()
+        object SEMICOLON : Symbol()
+        object LEFT_PAREN : Symbol()
+        object RIGHT_PAREN : Symbol()
+        object LEFT_BRACE : Symbol()
+        object RIGHT_BRACE : Symbol()
+        object COMMA : Symbol()
     }
 
     // literals
-    sealed interface Literal : TokenType {
-        object INTEGER : Literal
-        object STRING : Literal
+    sealed class Literal : TokenType() {
+        object INTEGER : Literal()
+        object STRING : Literal()
     }
 
     // identifiers
-    sealed interface Identifier : TokenType {
-        object IDENTIFIER : Identifier
+    sealed class Identifier : TokenType() {
+        object IDENTIFIER : Identifier()
     }
 
-    sealed interface Other : TokenType {
-        object EOF : Other
+    sealed class Other : TokenType() {
+        object EOF : Other()
     }
 }
